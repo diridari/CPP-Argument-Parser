c5c4f264a90e3a9fd6b444bd9f13ceb8ffcb9783
Unknown <balz@hm.edu>
2018-10-23 08:21:21 -0700

    git

---
+++ build/linux.mk
@@ -0,0 +1,38 @@
+#
+#  CONTENT
+#    common make rules for linux
+#
+#  AUTHOR
+#    Sebastian Balz
+#		based on Norbert Stoeffler
+#
+#  DATE
+#    2018-03-10
+#
+#  LANGUAGE
+#    make
+#
+
+CXXFLAGS +=		$(addprefix -I,$(APP_INCS)) -std=c++11
+_APP_OBJS1 = 	$(patsubst %.cc,%.o,$(patsubst %.cpp,%.o,$(APP_SRCS)))
+_APP_OBJS = 	$(notdir $(_APP_OBJS1))
+VPATH =			$(sort $(dir $(_APP_OBJS1)))
+
+all:: $(APP)
+
+clean::
+	rm -f $(APP) $(_APP_OBJS)
+
+run:	all
+	./$(APP)
+
+test:	all
+	./$(APP)
+
+$(APP): $(_APP_OBJS)
+	g++ -o $@ $^ $(addprefix -l,$(APP_SLIB))
+
+debug::
+	@echo APP_SRCS = $(APP_SRCS)
+	@echo _APP_OBJS = $(_APP_OBJS)
+	@echo VPATH=$(VPATH)
